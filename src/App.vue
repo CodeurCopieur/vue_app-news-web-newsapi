5b8ec0a920087a9beee50a98dba8bda9e9392b8d
codwerk <louis_widdy@live.fr>
2020-11-18 23:00:02 +0100

    delete component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <News :apiKey="apiKey"></News>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import News from './components/News.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    News
+  },
+  data() {
+    return {
+       apiKey: ''
+    }
   }
 }
 </script>
@@ -23,6 +27,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
