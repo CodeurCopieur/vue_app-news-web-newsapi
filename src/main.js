1843e534506530964e7200c5a757fc452196870d
codwerk <louis_widdy@live.fr>
2020-11-18 22:41:16 +0100

    import axios, bootstrap

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import bootstrap from 'bootstrap/dist/css/bootstrap.css'
 
-Vue.config.productionTip = false
+Vue.use(bootstrap);
+
+Vue.prototype.$http = axios
 
 new Vue({
   render: h => h(App),
