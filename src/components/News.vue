1e89c7f3f3cd58b99620dee819b12fccb60be425
codwerk <louis_widdy@live.fr>
2020-11-19 22:17:28 +0100

    intégration form

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,6 +1,18 @@
 <template>
-  <div class="container">
-    
+  <div class="container-fluid px-0" id="wrapper">
+    <div class="searchBar">
+      <form action="" class="h-100">
+        <input class="form-control form-control-lg rounded-0 bg-dark text-white border-0 shadow-none h-100" type="text" placeholder="Search">
+      </form>
+      <div class="searchIcons">
+        <i class="fas fa-search"></i>
+        <i class="fas fa-times"></i>
+      </div>
+    </div>
+
+    <div class="result-list">
+      
+    </div>
   </div>
 </template>
 
@@ -13,6 +25,39 @@
   }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+  
+  #wrapper {
+    position: relative;
+
+    .searchBar {
+      position: absolute;
+      width: 100%;
+      height: 60px;
+      font-size: 1.6rem;
+
+      form {
+        input {
+          font-size: 1.5rem;
+        }
+      }
+
+      .searchIcons {
+        position: absolute;
+        color: #fff;
+        right: 20px;
+        top: 50%;
+        transform: translateY(-50%);
+
+        i {
+          cursor: pointer;
+          &:nth-child(2) {
+            margin-left: 15px;
+          }
+        }
+      }
+    }  
+    
+    }
   
 </style>
