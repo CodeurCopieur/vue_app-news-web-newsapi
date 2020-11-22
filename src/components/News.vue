8307b003f5da61c55b4e76e387546bc458896170
codwerk <louis_widdy@live.fr>
2020-11-22 21:45:58 +0100

    intégration css(responsive)

--- src/components/News.vue
+++ src/components/News.vue
@@ -2,7 +2,7 @@
   <div class="container-fluid px-0" id="wrapper">
     <div class="searchBar position-relative">
       <form action="" class="h-100">
-        <input class="form-control form-control-lg rounded-0 bg-dark text-white border-0 shadow-none h-100" type="text" placeholder="Search" @input="fetchTopNews">
+        <input class="form-control form-control-lg rounded-0 bg-dark text-white border-0 shadow-none h-100" type="text" placeholder="Search" @input="fetchTopNews" v-model="searchword">
       </form>
       <div class="searchIcons">
         <i class="fas fa-search"></i>
@@ -11,16 +11,12 @@
     </div>
 
     <div class="container-lg d-flex flex-lg-row flex-wrap px-0 px-lg-2">
-      <article v-for="(article, index) in articles" :key="index" class="col-lg-4 col-md-6 px-0 px-lg-2 px-md-3">
-        <header>
-          <figure>
+      <article v-for="(article, index) in articles" :key="index" class="col-lg-4 col-md-6 px-0 px-lg-2 px-md-3 ">
+        <header class="">
             <img v-if="article.urlToImage" :src="article.urlToImage" class="img-fluid" alt="">
             <i v-else class="fas fa-image"></i>
-          </figure>
-          
-          
         </header>
-        <section v-html="article.title"></section>
+        <section v-html="article.title" class="wrap__content"></section>
         <footer>
           <i class="fas fa-chevron-right"></i>
         </footer>
@@ -42,7 +38,7 @@
        maxParPage: 20,
        searchword: '',
        articles: [],
-       country: 'fr'
+       country: 'us'
      }
    },
    methods: {
@@ -115,6 +111,30 @@
 
     article {
       cursor: pointer;
+
+      @media only screen and (max-width: 768px) {
+
+        display: grid;
+        grid-template-columns: 160px auto 40px;
+        grid-column-gap: 10px;
+        overflow: hidden;
+        align-items: center;
+
+        header {
+          display: flex;
+          justify-content: center;
+          overflow: hidden;
+        }
+        .wrap__content {
+          font-size: 10px;
+        }
+        footer {
+          i {
+            font-size: 1.6rem;
+            color: #888;
+          }
+        }
+      }
     }  
     
     }
