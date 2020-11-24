4b09cf45d692236da2bc0ec162c8190ec4303e41
codwerk <louis_widdy@live.fr>
2020-11-24 23:34:56 +0100

    fonctions(fetchsearchnews, scrolltrigger)

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="container-fluid px-0" id="wrapper">
     <div class="searchBar position-relative">
-      <form action="" class="h-100">
-        <input class="form-control form-control-lg rounded-0 bg-dark text-white border-0 shadow-none h-100" type="text" placeholder="Search" @input="fetchTopNews" v-model="searchword">
+      <form @submit.prevent="fetchSearchNews" class="h-100">
+        <input class="form-control form-control-lg rounded-0 bg-dark text-white border-0 shadow-none h-100" type="text" placeholder="Search" v-model="searchword">
       </form>
       <div class="searchIcons">
-        <i class="fas fa-search"></i>
-        <i class="fas fa-times"></i>
+        <i v-if="!isBusy" class="fas fa-search" @click="fetchSearchNews"></i>
+        <i v-else class="fas fa-spinner fa-spin"></i>
+        <i class="fas fa-times" @click="fetchTopNews"></i>
       </div>
     </div>
 
     <div class="container-lg d-flex flex-lg-row flex-wrap px-0 px-lg-2">
-      <article v-for="(article, index) in articles" :key="index" class="col-lg-4 col-md-6 px-0 px-lg-2 px-md-3 ">
+      <article v-for="(article, index) in articles" :key="index" class="col-lg-4 col-md-6 px-0 px-lg-2 px-md-3 " @click="navTo(article.url)">
         <header class="">
             <img v-if="article.urlToImage" :src="article.urlToImage" class="img-fluid" alt="">
             <i v-else class="fas fa-image"></i>
@@ -21,6 +22,9 @@
           <i class="fas fa-chevron-right"></i>
         </footer>
       </article>
+    </div>
+    <div ref="infiniteScrollerTrigger" id="scroll-trigger">
+      <i v-if="showloader" class="fas fa-spinner fas-spin"></i>
     </div>
   </div>
 </template>
@@ -41,10 +45,29 @@
        country: 'us'
      }
    },
+   computed: {
+     pageCount() {
+       return Math.ceil(this.totalResults/this.maxParPage);
+     }
+   },
    methods: {
+     navTo(url) {
+       window.open(url)
+     },
      resetData() {
        this.currentPage = 1;
        this.articles = [];
+     },
+     fetchSearchNews(){
+      if(this.searchword !== '') {
+        this.apiUrl = 'http://newsapi.org/v2/everything?q=' + this.searchword + '&pageSize=' + this.maxParPage + '&apiKey=' + this.apiKey;
+        this.isBusy = true;
+
+        this.resetData();
+        this.fetchData();
+      } else {
+        this.fetchTopNews();
+      }
      },
      fetchTopNews() {
        this.apiUrl = 'http://newsapi.org/v2/top-headlines?country=' + this.country + '&pageSize=' + this.maxParPage + '&apiKey=' + this.apiKey;
@@ -69,11 +92,26 @@
         .catch((error) => {
           console.log(error);
         })
+     },
+     scrollTriger() {
+       const observer = new IntersectionObserver( (entries) => {
+         entries.forEach( entry => {
+           if(entry.intersectionRatio > 0 && this.currentPage < this.pageCount) {
+             this.showloader = true;
+             this.currentPage += 1;
+             this.fetchData();
+           }
+         });
+       });
+       observer.observe(this.$refs.infiniteScrollerTrigger);
      }
    },
-     created() {
-       this.fetchTopNews()
-     }
+   created() {
+    this.fetchTopNews();
+   },
+   mounted() {
+    this.scrollTriger();
+   }
   }
 </script>
 
@@ -136,7 +174,14 @@
         }
       }
     }  
-    
+    #scroll-trigger {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      height: 100px;
+      font-size: 1.6rem;
     }
+  }
   
 </style>
